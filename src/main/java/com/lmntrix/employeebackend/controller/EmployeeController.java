a717b29ca942d7b21a686f3ec498e2ad8d4f60f9
kumarganeshmagam <kumarganeshmagam@gmail.com>
2024-01-11 22:16:14 +0530

    Updating controller to 4000 port

--- src/main/java/com/lmntrix/employeebackend/controller/EmployeeController.java
+++ src/main/java/com/lmntrix/employeebackend/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:4001")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
